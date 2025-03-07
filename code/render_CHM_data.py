6aceddce4d2201a23694387d544aa4b73e00a432
David Russell <djrussell@ucdavis.edu>
2025-03-07 14:36:54 +0000

    Add CLI to CHM rendering

--- code/render_CHM_data.py
+++ code/render_CHM_data.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pandas as pd
 import math
+import argparse
 
 from geograypher.meshes.derived_meshes import TexturedPhotogrammetryMeshChunked
 from geograypher.cameras import MetashapeCameraSet
@@ -11,38 +12,71 @@ PROCESSING_IDS_FILE = "/ofo-share/repos-david/UCNRS-experiments/data/processed_i
 ALL_IMAGES_FOLDER = "/ofo-share/drone-imagery-organization/3_sorted-notcleaned-combined"
 # Where the input photogrammetry products are
 PHOTOGRAMMETRY_PRODUCTS_FOLDER = "/ofo-share/repos-david/UCNRS-experiments/data/"
-# Where to save the results
-OUTPUT_FOLDER = "/ofo-share/repos-david/UCNRS-experiments/data/geograypher_outputs/CHM_pointcloud_renders_vis"
 
-# Should the dataset be skipped if the output folder exists already
-SKIP_EXISTING = True
-# This controls the fidelity of the rendering by rendering as if the image were captured at this
-# fraction of the original resolution. A lower value results in faster runtimes and less space on
-# disk, but a smoother, less detailed rendered CHM image.
-RENDER_SCALE = 0.1
-# Subsample the cameras to 1 out of this number. Useful for testing.
-TAKE_EVERY_NTH_CAMERA = 100
-# Show the rendered data alongside the original image. Useful for testing.
-MAKE_COMPOSITE = True
-# Should the mesh be shown
-VIS_MESH = True
-# The first dataset is very large, so it can be useful to skip it for experiments
-START_INDEX = 10
-# Should the pointcloud be used for the CHM values rather than the mesh
-USE_POINTCLOUD_CHM = False
-# How many cameras to include in single cluster, for which a portion of the mesh is cropped out.
-# It's unclear what the ideal number is, it balances the cost of a large mesh per cluster with
-# having to subset the mesh multiple times.
-N_CAMERAS_PER_CLUSTER = 100
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--output-folder", help="Where to save the results")
+    parser.add_argument(
+        "--skip-existing",
+        action="store_true",
+        help="Should the dataset be skipped if the output folder exists already",
+    )
+    parser.add_argument(
+        "--render-scale",
+        default=0.1,
+        type=float,
+        help="This controls the fidelity of the rendering by rendering as if the image were captured at this"
+        + "fraction of the original resolution. A lower value results in faster runtimes and less space on"
+        + "disk, but a smoother, less detailed rendered CHM image.",
+    )
+    parser.add_argument(
+        "--take-every-nth-camera",
+        default=100,
+        help="Subsample the cameras to 1 out of this number. Useful for testing.",
+    )
+    parser.add_argument(
+        "--make-composite",
+        action="store_true",
+        help="Show the rendered data alongside the original image. Useful for testing.",
+    )
+    parser.add_argument(
+        "--vis-mesh", action="store_true", help="Should the mesh be shown"
+    )
+    parser.add_argument(
+        "--start-index",
+        default=0,
+        help="The first dataset is very large, so it can be useful to skip it for experiments",
+    )
+    parser.add_argument(
+        "--use-pointcloud-CHM",
+        action="store_true",
+        help="Should the pointcloud be used for the CHM values rather than the mesh",
+    )
+    parser.add_argument(
+        "--n-cameras-per-cluster",
+        default=100,
+        type=int,
+        help="How many cameras to include in single cluster, for which a portion of the mesh is cropped out."
+        + "It's unclear what the ideal number is, it balances the cost of a large mesh per cluster with"
+        + "having to subset the mesh multiple times.",
+    )
+
+    args = parser.parse_args()
+    return args
 
 
 def render_chm(
     dataset_id,
     nrs_year,
+    output_folder,
     skip_existings=False,
-    vis=VIS_MESH,
-    take_every_nth_camera=TAKE_EVERY_NTH_CAMERA,
-    make_composite=MAKE_COMPOSITE,
+    n_cameras_per_cluster=100,
+    render_scale=0.1,
+    vis=False,
+    take_every_nth_camera=100,
+    make_composite=False,
     use_point_cloud_CHM=True,
 ):
     # Compute relavent paths based on dataset
@@ -63,7 +97,7 @@ def render_chm(
     )
 
     # Match the format of the input imagery for easy post-processing
-    rendered_CHMs_folder = Path(OUTPUT_FOLDER, f"{nrs_year}-ucnrs", dataset_id)
+    rendered_CHMs_folder = Path(output_folder, f"{nrs_year}-ucnrs", dataset_id)
 
     if not images_folder.is_dir():
         print(
@@ -105,13 +139,13 @@ def render_chm(
     # For large meshes it can dramatically speed up rendering to cluster the cameras and only render
     # the mesh within a threshold distance of that chunk. Select the number of chunks so there are
     # roughly N_CAMERAS_PER_CLUSTER cameras per chunk.
-    n_clusters = int(math.ceil(len(cameras) / N_CAMERAS_PER_CLUSTER))
+    n_clusters = int(math.ceil(len(cameras) / n_cameras_per_cluster))
 
     try:
         # Save out the renders of height-above-ground from each perspective
         mesh.save_renders(
             cameras,
-            render_image_scale=RENDER_SCALE,
+            render_image_scale=render_scale,
             output_folder=rendered_CHMs_folder,
             save_native_resolution=False,
             cast_to_uint8=False,
@@ -122,9 +156,11 @@ def render_chm(
         print(f"Dataset {dataset_id} failed with the following exception: {e}")
 
 
+args = parse_args()
+
 # Load the list of dataset IDs
 processing_ids = pd.read_csv(PROCESSING_IDS_FILE)
-for _, row in processing_ids.iloc[START_INDEX:, :].iterrows():
+for _, row in processing_ids.iloc[args.start_index :, :].iterrows():
     # Format the dataset ID
     dataset_id = f"{row.dataset_id:06}"
 
@@ -140,6 +176,12 @@ for _, row in processing_ids.iloc[START_INDEX:, :].iterrows():
     render_chm(
         dataset_id=dataset_id,
         nrs_year=nrs_year,
-        skip_existings=SKIP_EXISTING,
-        use_point_cloud_CHM=USE_POINTCLOUD_CHM,
+        output_folder=args.output_folder,
+        skip_existings=args.skip_existing,
+        n_cameras_per_cluster=args.n_cameras_per_cluster,
+        render_scale=args.render_scale,
+        vis=args.vis_mesh,
+        take_every_nth_camera=args.take_every_nth_camera,
+        make_composite=args.make_composite,
+        use_point_cloud_CHM=args.use_pointcloud_CHM,
     )
