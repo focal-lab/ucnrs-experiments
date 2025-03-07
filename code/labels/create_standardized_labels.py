1a84eba2ef015908dc2e20970208e9ef4f2ab114
David Russell <djrussell@ucdavis.edu>
2025-03-07 17:36:36 +0000

    First draft of remapping script

---
+++ code/labels/create_standardized_labels.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import json
+import shutil
+
+MAPPING_FILE = Path(
+    "/ofo-share/repos-david/UCNRS-experiments/data/subset_to_all_images_mapping.json"
+)
+
+LABELED_IMAGES_FOLDER = Path(
+    "/ofo-share/repos-david/UCNRS-experiments/data/labels/labeled_images_12_17_merged_classes"
+)
+STANDARDIZED_LABELED_IMAGES_FOLDER = Path(
+    "/ofo-share/repos-david/UCNRS-experiments/data/labels/labeled_images_12_17_merged_classes_standardized"
+)
+
+INITIAL_LOCATION = "/ofo-share/scratch-david/NRS_labeling/image_subset"
+
+FULL_FOLDER = "/ofo-share/drone-imagery-organization/3_sorted-notcleaned-combined"
+
+with open(MAPPING_FILE, "r") as infile:
+    mapping = json.load(infile)
+
+mapping = {Path(k).with_suffix(""): v for k, v in mapping.items()}
+
+files = sorted([f for f in (LABELED_IMAGES_FOLDER.rglob("*")) if f.is_file()])
+
+for file in files:
+    key = Path(
+        str(file).replace(str(LABELED_IMAGES_FOLDER), INITIAL_LOCATION)
+    ).with_suffix("")
+    if key in mapping:
+        value = mapping[key]
+        value = Path(
+            value.replace(str(FULL_FOLDER), str(STANDARDIZED_LABELED_IMAGES_FOLDER))
+        ).with_suffix(".png")
+        value.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copy(file, value)
+    else:
+        print(f"Skipping {key}")
