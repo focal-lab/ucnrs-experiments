c4cac92969944fae2a841b1a6ad5f70d4202f8f1
David Russell <djrussell@ucdavis.edu>
2025-05-09 15:00:10 +0000

    Streamline prediction projection
    Remove explicit dataset list and don't produce geospatial files in this step

--- code/prediction_post_processing/1_project_labels.py
+++ code/prediction_post_processing/1_project_labels.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-import pandas as pd
+import geopandas as gpd
+
 
 from geograypher.entrypoints.aggregate_images import aggregate_images
 
@@ -15,104 +16,38 @@ IDS_TO_LABELS = {
     7: "W_water",
 }
 
-PROCESSING_IDS_FILE = "/ofo-share/repos-david/UCNRS-experiments/data/processed_ids.csv"
-ALL_IMAGES_FOLDER = "/ofo-share/drone-imagery-organization/3_sorted-notcleaned-combined"
-DOWNLOADS_FOLDER = (
-    "/ofo-share/repos-david/UCNRS-experiments/data/photogrammetry_products"
+DATA_FOLDER = Path("/ofo-share/repos-david/UCNRS-experiments/data")
+ALL_IMAGES_FOLDER = Path(DATA_FOLDER, "inputs", "images")
+PHOTOGRAMMETRY_FOLDER = Path(DATA_FOLDER, "inputs", "photogrammetry")
+PER_IMAGE_PREDICTIONS_FOLDER = Path(
+    DATA_FOLDER, "intermediate", "per_image_predictions"
 )
-OUTPUT_FOLDER = "/ofo-share/repos-david/UCNRS-experiments/data/geograypher_outputs"
-ALL_LABELS_FOLDER = "/ofo-share/scratch-david/NRS-all-sites/preds_flipped"
+METADATA_FILE = Path(DATA_FOLDER, "inputs", "mission_metadata.gpkg")
+OUTPUT_FOLDER = Path(DATA_FOLDER, "intermediate", "geospatial_predictions")
 
 N_CAMERAS_PER_CHUNK = 100
-
 SKIP_EXISTING = True
 
-DATASET_IDS = [
-    544,
-    545,
-    546,
-    547,
-    548,
-    549,
-    551,
-    555,
-    557,
-    558,
-    563,
-    564,
-    565,
-    566,
-    567,
-    568,
-    570,
-    573,
-    574,
-    575,
-    576,
-    577,
-    578,
-    579,
-    580,
-    582,
-    583,
-    584,
-    585,
-    586,
-    587,
-    588,
-    1336,
-    1337,
-    610,
-    611,
-    612,
-    613,
-    614,
-    619,
-    620,
-    621,
-    622,
-    623,
-    627,
-    630,
-    908,
-    911,
-    912,
-    913,
-    919,
-    921,
-    924,
-    925,
-    926,
-    927,
-    928,
-    929,
-    930,
-    931,
-    559,
-    479,
-]
 
-
-def project_dataset(dataset_id, nrs_year, skip_existings=False):
+def project_dataset(dataset_id, skip_existings=False):
     # Compute relavent paths based on dataset
     # Path to the raw images
-    images_folder = Path(ALL_IMAGES_FOLDER, f"{nrs_year}-ucnrs", dataset_id)
+    images_folder = Path(ALL_IMAGES_FOLDER, dataset_id)
     # Path to the predictions from the model
-    labels_folder = Path(ALL_LABELS_FOLDER, f"ucnrs-{nrs_year}/{dataset_id}")
+    labels_folder = Path(PER_IMAGE_PREDICTIONS_FOLDER, dataset_id)
 
     # Path to input photogrammetry products
     mesh_file = Path(
-        DOWNLOADS_FOLDER, "mesh", f"mesh-internal-{dataset_id.lstrip('0')}.ply"
+        PHOTOGRAMMETRY_FOLDER, "mesh", f"mesh-internal-{dataset_id.lstrip('0')}.ply"
     )
     cameras_file = Path(
-        DOWNLOADS_FOLDER, "cameras", f"cameras-{dataset_id.lstrip('0')}.xml"
+        PHOTOGRAMMETRY_FOLDER, "cameras", f"cameras-{dataset_id.lstrip('0')}.xml"
     )
 
-    # Output files for the per-face and geospaital results
-    top_down_vector_projection_file = Path(
-        OUTPUT_FOLDER, "geospatial_maps", f"{dataset_id}.geojson"
+    # Output files for the per-face result
+    predicted_face_values_file = Path(
+        OUTPUT_FOLDER, "projections_to_faces", f"{dataset_id}.npy"
     )
-    predicted_face_values_file = Path(OUTPUT_FOLDER, "face_values", f"{dataset_id}.npy")
 
     # Check if labels are present
     if not Path(labels_folder).is_dir():
@@ -120,11 +55,7 @@ def project_dataset(dataset_id, nrs_year, skip_existings=False):
         return
 
     # If we're going to skip existing results, check if they have already been computed
-    if (
-        skip_existings
-        and predicted_face_values_file.is_file()
-        and top_down_vector_projection_file.is_file()
-    ):
+    if skip_existings and predicted_face_values_file.is_file():
         print(f"Dataset {dataset_id} exists already. Skipping")
         return
 
@@ -137,34 +68,18 @@ def project_dataset(dataset_id, nrs_year, skip_existings=False):
         labels_folder,
         IDs_to_labels=IDS_TO_LABELS,
         aggregated_face_values_savefile=predicted_face_values_file,
-        top_down_vector_projection_savefile=top_down_vector_projection_file,
         mesh_downsample=0.2,
         aggregate_image_scale=0.25,
         take_every_nth_camera=1,
         n_cameras_per_aggregation_cluster=N_CAMERAS_PER_CHUNK,
     )
-    # except Exception as e:
-    #    print(f"Dataset {dataset_id} failed with the following error {e}")
 
 
-# Load the list of dataset IDs
-processing_ids = pd.read_csv(PROCESSING_IDS_FILE)
+metadata = gpd.read_file(METADATA_FILE)
 
 # Loop over datasets
-for dataset_id in DATASET_IDS:
-    # row = processing_ids[processing_ids.dataset_id == dataset_id]
-    # row = row.iloc[0, :]
-    dataset_id = f"{dataset_id:06}"
-
-    if dataset_id <= "000588" or dataset_id >= "001336":
-        nrs_year = "2020"
-    elif dataset_id <= "000630":
-        nrs_year = "2023"
-    else:
-        nrs_year = "2024"
-
+for dataset_id in metadata.mission_id.values:
     project_dataset(
         dataset_id=dataset_id,
-        nrs_year=nrs_year,
         skip_existings=SKIP_EXISTING,
     )
