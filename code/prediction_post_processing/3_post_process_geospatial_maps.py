56b72f2b41f4b087f4a1644de95142b1db8a9076
David Russell <djrussell@ucdavis.edu>
2025-05-09 17:48:40 +0000

    update post processing step to use spatial-utils

--- code/prediction_post_processing/3_post_process_geospatial_maps.py
+++ code/prediction_post_processing/3_post_process_geospatial_maps.py
@@ -1,4 +1,3 @@
-# %%
 import geopandas as gpd
 import json
 import logging
@@ -8,24 +7,24 @@ from pathlib import Path
 from shapely import union, Geometry, MultiPolygon, make_valid, difference
 import numpy as np
 import typing
+from spatial_utils.geofileops_wrappers import (
+    geofileops_clip,
+    geofileops_simplify,
+    geofileops_buffer,
+)
+from spatial_utils.geometric import ensure_non_overlapping_polygons
 
 from pathlib import Path
 
-# %%
-GEOSPATIAL_MAPS_FOLDER = Path(
-    "/ofo-share/repos-david/UCNRS-experiments/data/geograypher_outputs/geospatial_maps_high_conf"
-)
-POST_PROCESSED_MAPS_FOLDER = Path(
-    "/ofo-share/repos-david/UCNRS-experiments/data/geograypher_outputs/geospatial_maps_post_processed_high_conf"
-)
-SHIFTED_MAPS_FOLDER = Path(
-    "/ofo-share/repos-david/UCNRS-experiments/data/final/post_processed_shifted_predictions_high_conf"
-)
 
-METADATA_FILE = "/ofo-share/drone-imagery-organization/3c_metadata-extracted/all-mission-polygons-w-metadata.gpkg"
-SHIFT_PER_DATASET = (
-    "/ofo-share/repos-david/UCNRS-experiments/data/shift_per_dataset.json"
-)
+DATA_FOLDER = Path("/ofo-share/repos-david/UCNRS-experiments/data")
+METADATA_FILE = Path(DATA_FOLDER, "inputs", "mission_metadata.gpkg")
+
+GEOSPATIAL_MAPS_FOLDER = Path(DATA_FOLDER, "intermediate", "geospatial_maps")
+POST_PROCESSED_MAPS_FOLDER = Path(DATA_FOLDER, "intermediate", "post_processed_maps")
+SHIFTED_MAPS_FOLDER = Path(DATA_FOLDER, "intermediate", "shifted_maps")
+
+SHIFTS_PER_DATASET = Path(DATA_FOLDER, "intermediate", "shift_per_dataset.json")
 
 # Simplify the geometry such that the maximum deviation never exceeds this amount
 SIMPLIFY_TOL = 0.1
@@ -34,125 +33,46 @@ VIS = False
 
 SKIP_EXISTING = True
 
-METADATA_FILE = "/ofo-share/drone-imagery-organization/3c_metadata-extracted/all-mission-polygons-w-metadata.gpkg"
 SCRATCH_FOLDER = Path("/ofo-share/repos-david/UCNRS-experiments/scratch/geofileops")
 BOUNDARY = Path(SCRATCH_FOLDER, "boundary.gpkg")
 
-INPUT_DATA_GPKG = Path(SCRATCH_FOLDER, "input.gpkg")
-SIMPLIFIED_1 = Path(SCRATCH_FOLDER, "simplified_1.gpkg")
-SIMPLIFIED_2 = Path(SCRATCH_FOLDER, "simplified_2.gpkg")
-BUFFERED_1 = Path(SCRATCH_FOLDER, "buffered_1.gpkg")
-BUFFERED_2 = Path(SCRATCH_FOLDER, "buffered_2.gpkg")
-BUFFERED_3 = Path(SCRATCH_FOLDER, "buffered_3.gpkg")
-NONOVERLAPPING = Path(SCRATCH_FOLDER, "nonoverlapping.gpkg")
-CLIPPED = Path(SCRATCH_FOLDER, "clipped.gpkg")
 
-# Allows you to specify a subset of the data for testing
-START_IND = 0
-STOP_IND = 100
-
-
-def ensure_non_overlapping_polygons(
-    geometries: typing.Union[typing.List[Geometry], gpd.GeoDataFrame],
-    inplace: bool = False,
-):
-    # Make sure geometries is a list of shapely objects
-    if isinstance(geometries, gpd.GeoDataFrame):
-        original_gdf = geometries
-        geometries = geometries.geometry.tolist()
-    else:
-        original_gdf = None
-
-    output_geometries = [None] * len(geometries)
-    union_of_added_geoms = MultiPolygon()
-
-    areas = [geom.area for geom in geometries]
-    sorted_inds = np.argsort(areas)
-
-    for ind in sorted_inds:
-        print(f"Processing ind {ind}")
-        # Get the input geometry and ensure it's valid
-        geom = make_valid(geometries[ind])
-        # Subtract the union of all
-        geom_to_add = difference(geom, union_of_added_geoms)
-        output_geometries[ind] = geom_to_add
-        # Add the original geom, not the difference'd one, to avoid boundary artifacts
-        union_of_added_geoms = union(geom, union_of_added_geoms)
-
-    if original_gdf is None:
-        return output_geometries
-    elif inplace:
-        original_gdf.geometry = output_geometries
-    else:
-        output_gdf = original_gdf.copy()
-        output_gdf.geometry = output_geometries
-        return output_gdf
-
-
-def post_process_gfo(dataset_id, input_path, output_path, working_crs=3310):
-    metadata = gpd.read_file(METADATA_FILE)
-    metadata_for_mission = metadata.query("@dataset_id == mission_id")
-    metadata_for_mission.to_file(BOUNDARY)
+def post_process_gfo(dataset_id, input_path, output_path, metadata):
 
     logging.basicConfig(level=logging.INFO)
     input_data = gpd.read_file(input_path)
-    input_data.to_crs(working_crs, inplace=True)
-    input_data.to_file(INPUT_DATA_GPKG)
-
-    gfo.simplify(
-        input_path=INPUT_DATA_GPKG,
-        output_path=SIMPLIFIED_1,
-        tolerance=SIMPLIFY_TOL,
-        force=True,
+    simplified1 = geofileops_simplify(
+        input_data, tolerence=SIMPLIFY_TOL, convert_to_projected_CRS=True
     )
-    gfo.buffer(
-        input_path=SIMPLIFIED_1,
-        output_path=BUFFERED_1,
-        distance=-BUFFER_AMOUNT,
-        endcap_style=gfo.BufferEndCapStyle.FLAT,
-        join_style=gfo.BufferJoinStyle.MITRE,
-        force=True,
+    buffered1 = geofileops_buffer(
+        simplified1, distance=-1 * BUFFER_AMOUNT, convert_to_projected_CRS=True
     )
-    gfo.buffer(
-        input_path=BUFFERED_1,
-        output_path=BUFFERED_2,
-        distance=2 * BUFFER_AMOUNT,
-        endcap_style=gfo.BufferEndCapStyle.FLAT,
-        join_style=gfo.BufferJoinStyle.MITRE,
-        force=True,
+    buffered2 = geofileops_buffer(
+        buffered1, distance=2 * BUFFER_AMOUNT, convert_to_projected_CRS=True
     )
-    gfo.buffer(
-        input_path=BUFFERED_2,
-        output_path=BUFFERED_3,
-        distance=-BUFFER_AMOUNT,
-        endcap_style=gfo.BufferEndCapStyle.FLAT,
-        join_style=gfo.BufferJoinStyle.MITRE,
-        force=True,
+    buffered3 = geofileops_buffer(
+        buffered2, distance=-1 * BUFFER_AMOUNT, convert_to_projected_CRS=True
     )
-    gfo.simplify(
-        input_path=BUFFERED_3,
-        output_path=SIMPLIFIED_2,
-        tolerance=SIMPLIFY_TOL,
-        force=True,
+    simplified2 = geofileops_simplify(
+        buffered3, tolerence=SIMPLIFY_TOL, convert_to_projected_CRS=True
     )
 
-    simplified = gpd.read_file(SIMPLIFIED_2)
-    nonoverlapping = ensure_non_overlapping_polygons(simplified)
-    nonoverlapping.to_crs(metadata_for_mission.crs, inplace=True)
-    nonoverlapping.to_file(NONOVERLAPPING)
+    nonoverlapping = ensure_non_overlapping_polygons(simplified2)
 
-    gfo.clip(input_path=NONOVERLAPPING, clip_path=BOUNDARY, output_path=output_path)
+    # TODO determine if it's cheaper to do this operation up front before simplifcation. The one
+    # downside is the boundary might not be as precise. So it might be important to do it both
+    # before and after.
+    # Extract the metadata for this mission
+    metadata_for_mission = metadata.query("@dataset_id == mission_id")
+    clipped = geofileops_clip(nonoverlapping, metadata_for_mission)
+
+    clipped.to_file(output_path)
 
 
-# %%
-# Load the metadata for all missions, which includes the flight polygons
 metadata_for_missions = gpd.read_file(METADATA_FILE)
-metadata_for_missions
+map_files = sorted(GEOSPATIAL_MAPS_FOLDER.glob("*"))
 
-# %%
-# List all the files that are present
-map_files = sorted(GEOSPATIAL_MAPS_FOLDER.glob("*"))[START_IND:STOP_IND]
-
+POST_PROCESSED_MAPS_FOLDER.mkdir(exist_ok=True, parents=True)
 for map_file in map_files:
     dataset_id = map_file.stem
 
@@ -166,16 +86,18 @@ for map_file in map_files:
     print(f"Postprocessing {dataset_id}")
 
     post_process_gfo(
-        dataset_id=dataset_id, input_path=map_file, output_path=output_file
+        dataset_id=dataset_id,
+        input_path=map_file,
+        output_path=output_file,
+        metadata=metadata_for_missions,
     )
 
-# %%
-# List all the files that are present
-map_files = sorted(POST_PROCESSED_MAPS_FOLDER.glob("*"))[START_IND:STOP_IND]
+map_files = sorted(POST_PROCESSED_MAPS_FOLDER.glob("*"))
 
-with open(SHIFT_PER_DATASET, "r") as infile:
+with open(SHIFTS_PER_DATASET, "r") as infile:
     shifts_per_dataset = json.load(infile)
 
+SHIFTED_MAPS_FOLDER.mkdir(exist_ok=True, parents=True)
 for map_file in map_files:
     # Read the file
     pred = gpd.read_file(map_file)
