94cecfb2cd1947c60954ea95d935158765657052
Amritha Pallavoor <amrithasp02@gmail.com>
2025-05-13 22:11:11 +0000

    include batch_size arg in inference script

--- run_inference.sh
+++ run_inference.sh
@@ -9,10 +9,11 @@ CONFIG_PATH="$1"
 CHECKPOINT_PATH="$2"
 IMAGE_FOLDER="$3"
 OUTPUT_FOLDER="$4"
+BATCH_SIZE="$5"
 
 # Configuration variables
 CONDA_ENV_NAME="openmmlab"
-MMSEG_DIR="/ofo-share/repos-amritha/mmsegmentation"  # Path to your local clone of https://github.com/open-forest-observatory/mmsegmentation.git
+MMSEG_DIR="/path/to/mmsegmentation"  # Path to your local clone of https://github.com/open-forest-observatory/mmsegmentation.git
 
 # Activate conda environment. Ensure conda is added to PATH.
 echo "Activating conda environment: $CONDA_ENV_NAME"
@@ -26,5 +27,6 @@ python inference.py \
     "$CONFIG_PATH" \
     "$CHECKPOINT_PATH" \
     "$IMAGE_FOLDER" \
-    "$OUTPUT_FOLDER"
+    "$OUTPUT_FOLDER" \
+    --batch-size "$BATCH_SIZE"
 echo "Inference complete. Results saved in: $OUTPUT_FOLDER"
