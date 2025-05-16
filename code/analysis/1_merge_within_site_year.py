f6102ae3ddbec75c02c86a6f1c4271e37fa1659a
David Russell <djrussell@ucdavis.edu>
2025-05-16 16:47:57 +0000

    Wrap in __name__ == __main__ to resove multiprocessing issue

--- code/analysis/1_merge_within_site_year.py
+++ code/analysis/1_merge_within_site_year.py
@@ -54,11 +54,14 @@ def compute_merged(
     """
     Crop the predictions to the shared region, merge all instances of the same class, and optional
     remove any overlaps between classes"""
+    # If there are no predictions, just return
     if len(preds) == 0:
         return None
 
     # Make sure everything is in a projected CRS
     preds = ensure_projected_CRS(preds)
+    # Lower the precision of the geometry to make future operations faster
+    preds.geometry = preds.set_precision(grid_size=0.01)
     # Clip the geometry to the shared region
     print("About to clip")
     clipped = geofileops_clip(preds, shared_region)
@@ -82,11 +85,7 @@ def compute_merged(
     # Clean up the geometry
     subset.geometry = subset.buffer(0)
     # Compute the area fraction
-    total_area = (
-        gpd.GeoDataFrame(data={"geometry": shared_region})
-        .to_crs(subset.crs)
-        .area.values[0]
-    )
+    total_area = shared_region.to_crs(subset.crs).area.values[0]
 
     subset["area_fraction"] = subset.area / total_area
 
@@ -95,102 +94,110 @@ def compute_merged(
     subset.to_file(output_file)
 
 
-# Find all predicted maps in the folder
-pred_files = sorted(SHIFTED_MAPS_FOLDER.glob("*"))
-# Read the metadata to get additional attributes
-metadata = gpd.read_file(METADATA_FILE)
+# This all needs to be wrapped in this block because of weird issues with multiprocessing in geofileops
+# see: https://github.com/geofileops/geofileops/issues/466
+if __name__ == "__main__":
+    # Find all predicted maps in the folder
+    pred_files = sorted(SHIFTED_MAPS_FOLDER.glob("*"))
+    # Read the metadata to get additional attributes
+    metadata = gpd.read_file(METADATA_FILE)
 
-preds = []
-for pred_file in pred_files:
-    # Load the predicted map for one dataset
-    pred = gpd.read_file(pred_file)
-    # Add the mission ID information so it's tracked in the future
-    pred["mission_id"] = pred_file.stem
-    preds.append(pred)
+    preds = []
+    for pred_file in pred_files:
+        # Load the predicted map for one dataset
+        pred = gpd.read_file(pred_file)
+        # Add the mission ID information so it's tracked in the future
+        pred["mission_id"] = pred_file.stem
+        preds.append(pred)
 
-all_preds = pd.concat(preds)
-# All the preds are in lat-lon. Convert to a more appropriate CRS for geometric operations.
-all_preds = ensure_projected_CRS(all_preds)
-# Convert the class_ID field from float to int
-all_preds["class_ID"] = all_preds["class_ID"].astype(int)
+    all_preds = pd.concat(preds)
+    # All the preds are in lat-lon. Convert to a more appropriate CRS for geometric operations.
+    all_preds = ensure_projected_CRS(all_preds)
+    # Convert the class_ID field from float to int
+    all_preds["class_ID"] = all_preds["class_ID"].astype(int)
 
-# Retain only the date from hte metadata
-metadata = metadata[["mission_id", "earliest_datetime_local_derived"]]
-metadata["earliest_datetime_local_derived"] = pd.to_datetime(
-    metadata["earliest_datetime_local_derived"]
-)
-# Ensure that both columns are the same type
-metadata["mission_id"] = metadata["mission_id"].astype(int)
-all_preds["mission_id"] = all_preds["mission_id"].astype(int)
-# Add the date column to the predictions
-all_preds = all_preds.merge(metadata, on="mission_id")
-
-# Restrict to the time period that leaves would be one
-if ONLY_LEAF_ON:
-    # Extract the mmdd representation of the month and day
-    int_month_day = (
-        all_preds["earliest_datetime_local_derived"].dt.strftime("%m%d").astype(int)
+    # Retain only the date from hte metadata
+    metadata = metadata[["mission_id", "earliest_datetime_local_derived"]]
+    metadata["earliest_datetime_local_derived"] = pd.to_datetime(
+        metadata["earliest_datetime_local_derived"]
     )
+    # Ensure that both columns are the same type
+    metadata["mission_id"] = metadata["mission_id"].astype(int)
+    all_preds["mission_id"] = all_preds["mission_id"].astype(int)
+    # Add the date column to the predictions
+    all_preds = all_preds.merge(metadata, on="mission_id")
 
-    index = (int_month_day > LEAF_ON_START_DATE) & (int_month_day < LEAF_ON_END_DATE)
+    # Restrict to the time period that leaves would be one
+    if ONLY_LEAF_ON:
+        # Extract the mmdd representation of the month and day
+        int_month_day = (
+            all_preds["earliest_datetime_local_derived"].dt.strftime("%m%d").astype(int)
+        )
 
-    print(f"{len(index) - index.sum()} rows that were leaf off were dropped")
-    all_preds = all_preds[index]
+        index = (int_month_day > LEAF_ON_START_DATE) & (
+            int_month_day < LEAF_ON_END_DATE
+        )
 
-# Add the information about which reserve it corresponds to
-all_preds = gpd.sjoin(all_preds, RESERVE_BOUNDS, how="left", predicate="intersects")
+        print(f"{len(index) - index.sum()} rows that were leaf off were dropped")
+        all_preds = all_preds[index]
 
-for reserve in RESERVES:
-    # Select data from only the reserve in question
-    reserve_preds = all_preds[all_preds["reserve"] == reserve]
-    # Add useful attributes
-    reserve_preds["year"] = reserve_preds["earliest_datetime_local_derived"].dt.year
-    reserve_preds["is_2020"] = reserve_preds["year"] == 2020
+    # Add the information about which reserve it corresponds to
+    all_preds = gpd.sjoin(all_preds, RESERVE_BOUNDS, how="left", predicate="intersects")
 
-    # Get the bounds of the predictions for each year
-    per_year_bounds = reserve_preds.dissolve(by="year", as_index=False)
+    for reserve in RESERVES:
+        # Select data from only the reserve in question
+        reserve_preds = all_preds[all_preds["reserve"] == reserve]
+        # Add useful attributes
+        reserve_preds["year"] = reserve_preds["earliest_datetime_local_derived"].dt.year
+        reserve_preds["is_2020"] = reserve_preds["year"] == 2020
 
-    # Find the areas that are present in all years for which any data is available
-    shared_region_separate_years = gpd.GeoDataFrame(
-        geometry=[per_year_bounds.geometry.intersection_all()], crs=per_year_bounds.crs
-    )
-    # Perform the same operation, but first merge 2023 and 2024
-    shared_region_merged_years = gpd.GeoDataFrame(
-        geometry=[per_year_bounds.dissolve("is_2020").geometry.intersection_all()],
-        crs=per_year_bounds.crs,
-    )
+        # Get the bounds of the predictions for each year
+        per_year_bounds = reserve_preds.dissolve(by="year", as_index=False)
 
-    # Compute the merged versions, separated by individual years
-    compute_merged(
-        reserve_preds[reserve_preds.year == 2020],
-        shared_region_separate_years,
-        output_file=Path(MERGED_MAPS_FOLDER, f"{reserve}_2020_separate_years.gpkg"),
-        ensure_nonoverlapping=ENSURE_NONOVERLAPPING,
-    )
-    compute_merged(
-        reserve_preds[reserve_preds.year == 2023],
-        shared_region_separate_years,
-        output_file=Path(MERGED_MAPS_FOLDER, f"{reserve}_2023_separate_years.gpkg"),
-        ensure_nonoverlapping=ENSURE_NONOVERLAPPING,
-    )
-    compute_merged(
-        reserve_preds[reserve_preds.year == 2024],
-        shared_region_separate_years,
-        output_file=Path(MERGED_MAPS_FOLDER, f"{reserve}_2024_separate_years.gpkg"),
-        ensure_nonoverlapping=ENSURE_NONOVERLAPPING,
-    )
+        # Find the areas that are present in all years for which any data is available
+        shared_region_separate_years = gpd.GeoDataFrame(
+            geometry=[per_year_bounds.geometry.intersection_all()],
+            crs=per_year_bounds.crs,
+        )
+        # Perform the same operation, but first merge 2023 and 2024
+        shared_region_merged_years = gpd.GeoDataFrame(
+            geometry=[per_year_bounds.dissolve("is_2020").geometry.intersection_all()],
+            crs=per_year_bounds.crs,
+        )
 
-    # Compute merged for the predictions with 2023 and 2024 merged
-    compute_merged(
-        reserve_preds[reserve_preds.is_2020],
-        shared_region_merged_years,
-        output_file=Path(MERGED_MAPS_FOLDER, f"{reserve}_2020_merged_years.gpkg"),
-        ensure_nonoverlapping=ENSURE_NONOVERLAPPING,
-    )
+        # Compute the merged versions, separated by individual years
+        compute_merged(
+            reserve_preds[reserve_preds.year == 2020],
+            shared_region_separate_years,
+            output_file=Path(MERGED_MAPS_FOLDER, f"{reserve}_2020_separate_years.gpkg"),
+            ensure_nonoverlapping=ENSURE_NONOVERLAPPING,
+        )
+        compute_merged(
+            reserve_preds[reserve_preds.year == 2023],
+            shared_region_separate_years,
+            output_file=Path(MERGED_MAPS_FOLDER, f"{reserve}_2023_separate_years.gpkg"),
+            ensure_nonoverlapping=ENSURE_NONOVERLAPPING,
+        )
+        compute_merged(
+            reserve_preds[reserve_preds.year == 2024],
+            shared_region_separate_years,
+            output_file=Path(MERGED_MAPS_FOLDER, f"{reserve}_2024_separate_years.gpkg"),
+            ensure_nonoverlapping=ENSURE_NONOVERLAPPING,
+        )
 
-    compute_merged(
-        reserve_preds[~reserve_preds.is_2020],
-        shared_region_merged_years,
-        output_file=Path(MERGED_MAPS_FOLDER, f"{reserve}_2023_2024_merged_years.gpkg"),
-        ensure_nonoverlapping=ENSURE_NONOVERLAPPING,
-    )
+        # Compute merged for the predictions with 2023 and 2024 merged
+        compute_merged(
+            reserve_preds[reserve_preds.is_2020],
+            shared_region_merged_years,
+            output_file=Path(MERGED_MAPS_FOLDER, f"{reserve}_2020_merged_years.gpkg"),
+            ensure_nonoverlapping=ENSURE_NONOVERLAPPING,
+        )
+
+        compute_merged(
+            reserve_preds[~reserve_preds.is_2020],
+            shared_region_merged_years,
+            output_file=Path(
+                MERGED_MAPS_FOLDER, f"{reserve}_2023_2024_merged_years.gpkg"
+            ),
+            ensure_nonoverlapping=ENSURE_NONOVERLAPPING,
+        )
